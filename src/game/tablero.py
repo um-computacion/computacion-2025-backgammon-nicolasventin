f06c9cb0193e8e52e2f4cb8627c73e1d58d199e4
Nico <nve2004@gmail.com>
2025-08-28 15:54:12 -0300

    agrego posiciones iniciales al tablero

--- src/game/tablero.py
+++ src/game/tablero.py
@@ -1,3 +1,29 @@
 class Tablero:
     def __init__(self):
-        ...
+        self.__turnos__ = 0
+        self.__posiciones__ = [
+            (2, "Blancas"),
+            (0, None), 
+            (0, None), 
+            (0, None), 
+            (0, None), 
+            (5, "Negras"),
+            (0, None), 
+            (3, "Negras"), 
+            (0, None), 
+            (0, None), 
+            (0, None), 
+            (5, "Blancas"),
+            (5, "Negras"), 
+            (0, None), 
+            (0, None), 
+            (0, None), 
+            (3, "Blancas"), 
+            (0, None),
+            (5, "Blancas"), 
+            (0, None), 
+            (0, None), 
+            (0, None), 
+            (0, None), 
+            (2, "Negras")
+        ]
