155b2629be98e277d22904cb7f5e681785ce9845
Nico <nve2004@gmail.com>
2025-09-01 20:54:54 -0300

    agrego test basicos tablero

--- tests/test_tablero.py
+++ tests/test_tablero.py
@@ -8,6 +8,38 @@ class TestTablero(unittest.TestCase):
     def test__turnos__(self):
         self.assertEqual(self.tablero.__turnos__, 0)
 
+    def test__cantidad_de_posiciones__(self):
+        self.assertEqual(self.tablero.__cantidad_de_posiciones__, 24)
+
+    def test_posiciones_iniciales(self):
+        posiciones_esperadas = [
+            (2, "Blancas"),
+            (0, None), 
+            (0, None), 
+            (0, None), 
+            (0, None), 
+            (5, "Negras"),
+            (0, None), 
+            (3, "Negras"), 
+            (0, None), 
+            (0, None), 
+            (0, None), 
+            (5, "Blancas"),
+            (5, "Negras"), 
+            (0, None), 
+            (0, None), 
+            (0, None), 
+            (3, "Blancas"), 
+            (0, None),
+            (5, "Blancas"), 
+            (0, None), 
+            (0, None), 
+            (0, None), 
+            (0, None), 
+            (2, "Negras")
+        ]
+        self.assertEqual(self.tablero._Tablero__posiciones__, posiciones_esperadas)
+
 
 if __name__ == '__main__':  
     unittest.main()
