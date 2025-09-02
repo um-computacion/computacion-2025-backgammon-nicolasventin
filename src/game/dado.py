52f7905167862b43f5188d2d97a13ce7d028a586
Nico <nve2004@gmail.com>
2025-09-02 19:17:11 -0300

    Agrego funcion tirar dados

--- src/game/dado.py
+++ src/game/dado.py
@@ -4,3 +4,13 @@ class Dado:
     def __init__(self):
         self.dado1 = 0
         self.dado2 = 0
+        self.resultados = []
+
+    def tirar(self):
+        resultado1 = self.dado1.tirar()
+        resultado2 = self.dado2.tirar()
+        
+        if resultado1 == resultado2:
+            self.resultados = [resultado1] * 4
+        else:
+            self.resultados = [resultado1, resultado2]
