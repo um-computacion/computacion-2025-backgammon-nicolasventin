e134899a1a0daa2fe9aaaf2bbdbe7bf255ae5c6c
Nico <nve2004@gmail.com>
2025-09-14 19:19:16 -0300

    cambio la configuracion inicial de fichas

--- src/game/tablero.py
+++ src/game/tablero.py
@@ -1,30 +1,14 @@
 class Tablero:
     def __init__(self):
         self.__turnos__ = 0
-        self.__cantidad_de_posiciones__ = 24
-        self.__posiciones__ = [
-            (2, "Blancas"),
-            (0, None), 
-            (0, None), 
-            (0, None), 
-            (0, None),
-            (5, "Negras"), 
-            (0, None), 
-            (3, "Negras"), 
-            (0, None), 
-            (0, None), 
-            (0, None), 
-            (5, "Blancas"),
-            (5, "Negras"), 
-            (0, None), 
-            (0, None), 
-            (0, None), 
-            (3, "Blancas"), 
-            (0, None),
-            (5, "Blancas"), 
-            (0, None), 
-            (0, None), 
-            (0, None), 
-            (0, None), 
-            (2, "Negras")
-        ]
+        self.__puntos__ = [0] * 24
+
+        self.__puntos__[0] =  2    # 2 blancas
+        self.__puntos__[11] = 5    # 5 blancas
+        self.__puntos__[16] = 3    # 3 blancas
+        self.__puntos__[18] = 5    # 5 blancas
+
+        self.__puntos__[23] = -2   # 2 negras
+        self.__puntos__[12] = -5   # 5 negras
+        self.__puntos__[7]  = -3   # 3 negras
+        self.__puntos__[5]  = -5   # 5 negras
