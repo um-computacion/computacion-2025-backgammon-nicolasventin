cbe35092b445e5007f7dcaf282298f1a785e1ec6
Nico <nve2004@gmail.com>
2025-09-14 19:32:54 -0300

    cambio test de tablero

--- tests/test_tablero.py
+++ tests/test_tablero.py
@@ -9,36 +9,20 @@ class TestTablero(unittest.TestCase):
         self.assertEqual(self.tablero.__turnos__, 0)
 
     def test__cantidad_de_posiciones__(self):
-        self.assertEqual(self.tablero.__cantidad_de_posiciones__, 24)
+        self.assertEqual(len(self.tablero._Tablero__puntos__), 24)
 
     def test_posiciones_iniciales(self):
-        posiciones_esperadas = [
-            (2, "Blancas"),
-            (0, None), 
-            (0, None), 
-            (0, None), 
-            (0, None), 
-            (5, "Negras"),
-            (0, None), 
-            (3, "Negras"), 
-            (0, None), 
-            (0, None), 
-            (0, None), 
-            (5, "Blancas"),
-            (5, "Negras"), 
-            (0, None), 
-            (0, None), 
-            (0, None), 
-            (3, "Blancas"), 
-            (0, None),
-            (5, "Blancas"), 
-            (0, None), 
-            (0, None), 
-            (0, None), 
-            (0, None), 
-            (2, "Negras")
-        ]
-        self.assertEqual(self.tablero._Tablero__posiciones__, posiciones_esperadas)
+        posiciones_esperadas = [0] * 24
+        posiciones_esperadas[0]  =  2
+        posiciones_esperadas[11] =  5
+        posiciones_esperadas[16] =  3
+        posiciones_esperadas[18] =  5
+        posiciones_esperadas[23] = -2
+        posiciones_esperadas[12] = -5
+        posiciones_esperadas[7]  = -3
+        posiciones_esperadas[5]  = -5
+
+        self.assertEqual(self.tablero._Tablero__puntos__, posiciones_esperadas)
 
 
 if __name__ == '__main__':  
