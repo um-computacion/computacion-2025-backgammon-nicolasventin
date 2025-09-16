b8c47162a3292aba02fad5186ef184bacd885954
Nico <nve2004@gmail.com>
2025-09-16 16:44:06 -0300

    Agrego test de obtener info de jugador

--- tests/test_jugador.py
+++ tests/test_jugador.py
@@ -4,6 +4,11 @@ from src.game.jugador import Jugador
 class TestJugador(unittest.TestCase):
     
     def test_crear_jugador(self):
-        jugador = Jugador("Diego", "Blanca")
+        jugador = Jugador("Diego", "Blancas")
         self.assertEqual(jugador.nombre, "Diego")
-        self.assertEqual(jugador.ficha, "Blanca")
+        self.assertEqual(jugador.ficha, "Blancas")
+
+    def test_obtener_info(self):
+        jugador = Jugador("Camila", "Negras")
+        resultado = jugador.obtener_info()
+        self.assertEqual(resultado, "Camila (Negras)")
