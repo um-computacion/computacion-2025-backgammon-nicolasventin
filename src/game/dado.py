56bafe42a9dd6ed720084651d08f8a10a9817eb1
Nico <nve2004@gmail.com>
2025-10-08 23:20:58 -0300

    Agrego clase Dice

--- src/game/dado.py
+++ src/game/dado.py
@@ -1,12 +1,16 @@
 import random
 
-def get_dice():
-    try:
-        dice_0 = random.randint(1, 6)
-        dice_1 = random.randint(1, 6)
-        if dice_0 == dice_1:
-            return  (dice_0, dice_1, dice_0, dice_1, )
-        else:
-            return  (dice_0, dice_1, )
-    except Exception as e:
-        return ()           
+class Dice:
+    def __init__(self):
+        self.__values__ = []
+
+    def get_dice():
+        try:
+            dice_0 = random.randint(1, 6)
+            dice_1 = random.randint(1, 6)
+            if dice_0 == dice_1:
+                return  (dice_0, dice_1, dice_0, dice_1, )
+            else:
+                return  (dice_0, dice_1, )
+        except Exception as e:
+            return ()           
