85799353a876a6fab8b1dc9e701c85493caf9b0c
Nico <nve2004@gmail.com>
2025-10-08 23:25:02 -0300

    agrego algunas actualizaciones

--- src/game/backgammon.py
+++ src/game/backgammon.py
@@ -1,7 +1,13 @@
 from src.game.tablero import Tablero
 from src.game.jugador import Jugador
+from src.game.dado import Dice
 
 class BackgammonGame:
     def __init__(self, name_p1="Jugador 1", name_p2="Jugador 2"):
         self._board = Tablero()
+        self.__dice__ = Dice()
+        self.__players__ = [
+            Jugador(name_p1, id_=1),
+            Jugador(name_p2, id_=-1)
+        ]
         pass
