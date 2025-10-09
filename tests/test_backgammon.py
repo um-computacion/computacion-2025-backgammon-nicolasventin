46501ea288f30d27915ed712bd64c8c24ca72728
Nico <nve2004@gmail.com>
2025-10-08 23:55:00 -0300

    agrego test turno inicial

--- tests/test_backgammon.py
+++ tests/test_backgammon.py
@@ -5,6 +5,8 @@ class TestBackgammonGame(unittest.TestCase):
     def setUp(self):
         self.game = BackgammonGame()
 
+    def test_turno_inicial(self):
+        self.assertEqual(self.game.__turno__, 0)
 
 if __name__ == '__main__':
     unittest.main()
