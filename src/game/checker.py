a2fd5f874aa76b02d1be8c6db5e64084bc258236
Nico <nve2004@gmail.com>
2025-10-13 19:51:59 -0300

    agrego logica de si esta comida la ficha

--- src/game/checker.py
+++ src/game/checker.py
@@ -1,6 +1,15 @@
 class Checker:
     def __init__(self, color: str) -> None:
         self.__color: str = color
+        self.__esta_comida = False
 
     def get_color(self) -> str:
         return self.__color
+
+    @property
+    def comida(self):
+        return self.__esta_comida
+    
+    @comida.setter
+    def set_comida(self, esta_comida: bool) -> None:
+        self.__esta_comida = esta_comida
