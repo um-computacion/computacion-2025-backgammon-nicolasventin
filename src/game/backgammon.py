c474abb44488cb13069d9a2b4553218fc43b0f3f
Nico <nve2004@gmail.com>
2025-10-23 17:20:02 -0300

    Aplico principios SOLID y estándares de código al proyecto.

--- src/game/backgammon.py
+++ src/game/backgammon.py
@@ -3,8 +3,10 @@ from src.game.jugador import Jugador
 from src.game.dado import Dice
 
 class BackgammonGame:
+    """Clase principal que maneja la lógica y el estado de la partida."""
     def __init__(self, name_p1="Jugador 1", name_p2="Jugador 2"):
-        self._board = Tablero()
+        """Inicializa el tablero, los dados, los jugadores y el turno."""
+        self.__board__ = Tablero()
         self.__dice__ = Dice()
         self.__players__ = [
             Jugador(name_p1, "B"),
