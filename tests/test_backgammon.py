195e5f8e3f7c5dc1ed07f4364c2e33fc368931c3
Nico <nve2004@gmail.com>
2025-10-28 18:39:22 -0300

    Agrego logica de bearing off y victoria a backgammon game

--- tests/test_backgammon.py
+++ tests/test_backgammon.py
@@ -63,6 +63,83 @@ class TestBackgammonGame(unittest.TestCase):
 
         self._setup_checkers(5, 'B', 1) 
         self.assertFalse(self.game.validar_movimiento(5, 1))
+    
+    def test_validar_movimiento_bear_off_exacto(self):
+        """Verifica que se permita salir con dado exacto si está en Home Board."""
+        self.game.__board__.__puntos__ = [[] for _ in range(24)]
+        self.game.__board__.__bar_blancas__ = []
+        self.game.__board__.__bar_negras__ = []
+        self._setup_checkers(2, 'B', 1)
+        self.game.__dados_restantes__ = [3]
+
+        self.assertTrue(self.game.validar_movimiento(2, -1))
+
+        self.game.__turno__ = 1
+        for i in range(0, 18): self.game.__board__.__puntos__[i] = []
+        self.game.__board__.__bar_blancas__ = []
+        self.game.__board__.__bar_negras__ = []
+        self._setup_checkers(21, 'N', 1)
+        self.game.__dados_restantes__ = [3]
+
+        self.assertTrue(self.game.validar_movimiento(21, 25))
+
+    def test_validar_movimiento_bear_off_dado_mayor_lejana(self):
+        """Verifica que se permita salir con dado mayor si es la ficha más lejana."""
+        self.game.__board__.__puntos__ = [[] for _ in range(24)]
+        self.game.__board__.__bar_blancas__ = []
+        self.game.__board__.__bar_negras__ = []
+        self._setup_checkers(2, 'B', 1)
+        self.game.__dados_restantes__ = [5]
+
+        self.assertTrue(self.game.validar_movimiento(2, -1))
+
+        self.game.__turno__ = 1
+        self.game.__board__.__puntos__ = [[] for _ in range(24)]
+        self.game.__board__.__bar_blancas__ = []
+        self.game.__board__.__bar_negras__ = []
+        self._setup_checkers(21, 'N', 1)
+        self.game.__dados_restantes__ = [5]
+
+        self.assertTrue(self.game.validar_movimiento(21, 25))
+
+    def test_validar_movimiento_bear_off_dado_mayor_no_lejana(self):
+        """Verifica que NO se permita salir con dado mayor si NO es la ficha más lejana."""
+        self.game.__board__.__puntos__ = [[] for _ in range(24)]
+        self._setup_checkers(2, 'B', 1)
+        self._setup_checkers(4, 'B', 1)
+        self.game.__dados_restantes__ = [5]
+
+        self.assertFalse(self.game.validar_movimiento(2, -1))
+
+    def test_validar_movimiento_bear_off_falla_si_no_home_board(self):
+        """Verifica que Bear Off falle si no todas las fichas están en Home Board."""
+        self._setup_checkers(6, 'B', 1)
+        self._setup_checkers(2, 'B', 1)
+        self.game.__dados_restantes__ = [3]
+
+        self.assertFalse(self.game.validar_movimiento(2, -1))
+
+    def test_ejecutar_movimiento_bear_off(self):
+        """Verifica que ejecutar_movimiento quite la ficha al hacer Bear Off."""
+        self.game.__board__.__puntos__ = [[] for _ in range(24)]
+        self._setup_checkers(2, 'B', 1)
+        self.game.__dados_restantes__ = [3]
+        self.game.ejecutar_movimiento(2, -1)
+
+        self.assertEqual(len(self.game.__board__.__puntos__[2]), 0)
+        self.assertEqual(self.game.__dados_restantes__, [])
+
+    def test_check_victory(self):
+        """Verifica la condición de victoria."""
+        self.game.__board__.__puntos__ = [[] for _ in range(24)]
+        self.game.__board__.__bar_blancas__ = []
+
+        self._setup_checkers(20, 'N', 1)
+
+        self.assertTrue(self.game.check_victory())
+
+        self.game.__turno__ = 1
+        self.assertFalse(self.game.check_victory())
 
 if __name__ == '__main__':
     unittest.main()
